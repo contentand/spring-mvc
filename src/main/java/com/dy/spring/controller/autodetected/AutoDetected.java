750d5ea1d889158cf78ee11a9df9ce3998b0b48f
Daniil Yurov <daniel.jurow@hotmail.com>
2016-10-02 21:26:56 +0300

    Mapping a Controller by Presence of Particular Parameter

--- src/main/java/com/dy/spring/controller/autodetected/AutoDetected.java
+++ src/main/java/com/dy/spring/controller/autodetected/AutoDetected.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -20,5 +21,11 @@ public class AutoDetected {
     @ResponseBody @RequestMapping(name = "/get", method = RequestMethod.GET)
     public String get() {
         return "Here you go!";
+    }
+
+    // By Presence of query parameter
+    @ResponseBody @RequestMapping(path = "/hi", params={"from"})
+    public String sayHiFrom(@RequestParam String from) {
+        return "Hi, " + name + "!<br>From " + from;
     }
 }
