e64dc3c8c25672178c7154113c43fcc5e6aa7165
Daniil Yurov <daniel.jurow@hotmail.com>
2016-10-02 21:22:03 +0300

    Mapping a Controller by HTTP Method call

--- src/main/java/com/dy/spring/controller/autodetected/AutoDetected.java
+++ src/main/java/com/dy/spring/controller/autodetected/AutoDetected.java
@@ -3,6 +3,7 @@ package com.dy.spring.controller.autodetected;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,5 +14,11 @@ public class AutoDetected {
     @ResponseBody @RequestMapping(value = "/hi")
     public String sayHi() {
         return "Hi, " + name + "!";
+    }
+
+    //By HTTP method for any UNMAPPED request. Note! /get is not url is a name!
+    @ResponseBody @RequestMapping(name = "/get", method = RequestMethod.GET)
+    public String get() {
+        return "Here you go!";
     }
 }
