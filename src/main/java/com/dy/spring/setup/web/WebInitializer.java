bca8b51bfb7a704c0417b64d4c452eb2582ab344
Daniil Yurov <daniel.jurow@hotmail.com>
2016-10-02 14:09:33 +0300

    Added Root Context

--- src/main/java/com/dy/spring/setup/web/WebInitializer.java
+++ src/main/java/com/dy/spring/setup/web/WebInitializer.java
@@ -1,6 +1,7 @@
 package com.dy.spring.setup.web;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
@@ -10,6 +11,11 @@ import javax.servlet.ServletRegistration;
 public class WebInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
+
+        servletContext.setInitParameter("contextConfigLocation", "/WEB-INF/spring-root/application-context.xml");
+
+        servletContext.addListener(ContextLoaderListener.class);
+
         ServletRegistration.Dynamic reg = servletContext.addServlet("front-controller",  DispatcherServlet.class);
         reg.setInitParameter("contextConfigLocation", "/WEB-INF/spring-web/front-controller.xml");
         reg.setLoadOnStartup(1);
